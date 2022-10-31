f15311e700ef828b086942a6bd584125eb245d27
Jesse <zheng.1708@osu.edu>
2022-10-31 12:09:50 -0400

    initial commit

--- abc.java
+++ abc.java
@@ -1,23 +1,49 @@
 abstract class A {
     private A h;
-    public A(A x) { this .h = x; if (x != null) x. set(this ); } // constructor
-    public A get() { return this .h; }
+    public A(A x) { 
+        this.h = x; 
+        if (x != null) 
+            x.set(this); 
+    } // constructor
+    public A get() { 
+        return this.h; 
+    }
     public void set(A y) {
-    this .h = y;
-    // time moment 1.5
+        this .h = y;
+        // time moment 1.5
+    }
+    public A geth() {
+        return this.h;
     }
     public abstract A p();
-    public static void q(A x) { A y; y = x.get(); A z; z = y.p(); }
+    public static void q(A x) { 
+        A y; 
+        y = x.get(); 
+        A z; 
+        z = y.p(); 
+    }
     public void r() { A x; x = this.get(); q(x); }
 }
 class B extends A {
-    public B(A x) { super(x); } // constructor : calls A(A x)
-    public A p() { return this ; }
+    public B(A x) { 
+        super(x); 
+    } // constructor : calls A(A x)
+    public A p() { 
+        return this; 
+    }
 }
 class C extends B {
-    public C(A x) { super(x); } // constructor ; calls B(A x)
-    public A get() { return this ; }
-    public A p() { A x; x = this.get(); return x; }
+    public C(A x) { 
+        super(x); 
+    } // constructor ; calls B(A x)
+    public A get() { 
+        return this; 
+    }
+    public A p() { 
+        A x; 
+        x = this.get(); 
+        return x; 
+    }
 }
 class Main {
     public static void main(String [] args) {
